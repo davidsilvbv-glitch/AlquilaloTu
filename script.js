258a1b4637568a5f6db78bd515e50454fa5c1e21
David <davidsilvbv@gmail.com>
2026-02-23 21:16:18 -0400

    Primera versión

---
+++ script.js
@@ -0,0 +1,14 @@
+// Reveal al hacer scroll
+const revealElements = document.querySelectorAll('.step, .impact-card, .card');
+
+window.addEventListener('scroll', () => {
+  revealElements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if(position < windowHeight - 100){
+      el.style.opacity = 1;
+      el.style.transform = "translateY(0)";
+    }
+  });
+});
